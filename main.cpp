2c1c55a95c09875c6e87045764757884ce5ecfe9
monopatovska <43369990+monopatovska@users.noreply.github.com>
2018-09-25 11:36:19 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,56 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	string str;
+	int n;
+
+	if (argc == 1)
+		cout << "Parameter nebol zadany" << endl;
+	if (argc == 2)
+	{
+		if (string(argv[argc - 1]) == "-c")
+		{
+			getline(std::cin, str);
+			//cout << str << endl;
+			//pocitanie znakov
+			n = str.length();
+		
+			cout << "Pocet znakov je "<< n << endl;
+		}
+
+		if (string(argv[argc - 1]) == "-l")
+		{
+			getline(std::cin, str);
+			//cout << str << endl;
+			//pocitanie slov
+			int poc = 1;
+			for (int i = 0; i < int(str.length()); i++)
+			{
+				if (isspace(str[i]))
+					poc++;
+			}
+
+			cout << "Pocet slov je " << poc << endl;
+		}
+
+		if (string(argv[argc - 1]) == "-w")
+		{
+			getline(std::cin, str);
+			cout << str << endl;
+			//pocitanie riadkov
+			
+		}
+	}
+	
+	system("pause");
 
 	return 0;
 }
